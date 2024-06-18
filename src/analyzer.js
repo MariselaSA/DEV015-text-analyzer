b2cc944a798ba0c2c2cb4c47cc92b3ea5a507b45
MariselaSA <mariselaestudy@gmail.com>
2024-06-18 12:15:17 -0500

    SUBIR PROYECTO

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,79 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (text) => { // parametros
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //console.log(text.length)
+    return text.split(' ').length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
+
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text.replace(/[\s\p{P}]/gu, '').length; // Usamos una expresión regular para eliminar espacios y signos de puntuación
+    // La expresión /[\s\p{P}]/gu busca todos los espacios (\s) y signos de puntuación (\p{P})
+    // La bandera 'u' es para soporte de Unicode y 'g' es para hacer la búsqueda global
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras= text.split(' ');
+    let sumaDeCaracteres=0;
+    for (let i = 0; i<palabras.length; i++) {
+    //console.log(palabras[i]);
+    //console.log("conteo de caracteres: " +  palabras[i].length);
+      sumaDeCaracteres += palabras[i].length;
+
+    // console.log("suma: " + sumaDeCaracteres);
+    }
+
+    const promedio = sumaDeCaracteres / palabras.length;
+
+    return promedio.toFixed(2); //Metodo que permita tenerTenga solo 2 decimales .toFixed 
+
+ 
+
   },
+  
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    if(text.match(/\d+/g) !== null){ //Aprendi que existe una diferencia entre el /\d+/g y el /\d/g el primero busca lo coincidencia de 
+      //números y los agrupo el segundo igualmente peor por separado
+      // console.log(text.match(/\d/g).length);
+      return text.match(/\b\d+(\.\d+)?\b/g).length;
+    }else{
+      return 0;
+    }            
+
+
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+    const numeros =text.match(/\b\d+(\.\d+)?\b/g);// Usar una expresión regular para encontrar todos los dígitos en el string creando una variable
+    /* -?: Coincide con un signo negativo opcional.
+    \d+: Coincide con una o más cifras (dígitos del 0 al 9).
+   (\.\d+)?: Coincide con un punto decimal seguido de una o más cifras, opcionalmente.
+    g: Indicador global, que asegura que se encuentran todas las coincidencias en la cadena, no solo la primera.*/
+    //console.log(numeros);
+        
+    if(!numeros){ //Si no se encuentran dígitos, devolver 0
+      return 0;
+    }else{          
+      const suma = numeros.reduce((acumulador, numero) => {  // Convertir los dígitos a enteros y sumarlos
+        const digito = parseFloat(numero); // Convertir el dígito (en formato string) a un entero
+        // console.log(acumulador);
+  
+        return acumulador + digito;// Sumar el dígito al acumulador
+  
+      }, 0);
+
+        
+      return suma;
+    }
+
+
+
+  }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 import analyzer from './analyzer.js';
+//Darle Evento al textarea//
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const userInput= document.querySelector('textarea[name="user-input"]');
+const lista = document.querySelectorAll("li"); // ARRAY
+ 
+ 
+//console.log(lista);
+//userInput.addEventListener('user-input', obtenerTexto );
+//Aquí cambie mi de input a user input
+/***FUNCION ANONIMA****/
+userInput.addEventListener('input', obtenerTexto);
+//     console.log(userInput.value)
+//  })
+
+//function obtenerTexto(){
+//     console.log(userInput.value)
+// }
+//const obtenerTexto = ()=>{
+// console.log(userInput.value)
+function obtenerTexto(){
+  /***OBJETOS EN JS***/
+  const resultWordCount = analyzer.getWordCount(userInput.value) //argumento
+  const resulCharacterCount = analyzer.getCharacterCount(userInput.value)
+  const resulCharacterCountExcludingSpaces =analyzer.getCharacterCountExcludingSpaces(userInput.value)
+  const resulAverageWordLength=analyzer.getAverageWordLength(userInput.value)
+  const resulNumberCount= analyzer.getNumberCount(userInput.value)
+  const resulNumberSum= analyzer.getNumberSum(userInput.value)
+  /***ARREGLOS/ARRAY EN JS****/
+  lista[0].textContent = "Recuento de Palabras:  " +  resultWordCount;
+  lista[1].textContent="Recuento de Carácteres:  " +  resulCharacterCount;
+  lista[2].textContent= "Recuento Sin Espacio y Signos: "  +resulCharacterCountExcludingSpaces;
+  lista[3].textContent ="Longitud media de Palabras : "+ resulAverageWordLength;
+  lista[4].textContent= "Recuento de Números :" + resulNumberCount;
+  lista[5].textContent="Suma de Números:" + resulNumberSum;
+}
+document.getElementById("reset-button").addEventListener('click', limpiar);
+function limpiar(){
+  userInput.value = '' ;
+
+  lista[0].textContent=" Recuento de Palabras: "; 
+  lista[1].textContent=" Recuento de Carácteres: "; 
+  lista[2].textContent=" Recuento Sin Espacio y Signos: "; 
+  lista[3].textContent="Longitud media de Palabras : ";
+  lista[4].textContent="Recuento de Números :" ;
+  lista[5].textContent="Suma de Números:" ;
+}
+
+    
+    
+     
+
+
+ 
+
+
+
+ 
+ 
+
+/****PARAMETROS Y ARGUMENTOS
+ * a + b = 8; // parametros
+ * 5 + 3 = 8; // argumentos""valorreal"
+ * ****/
+//Todo : Escuchar evetnso del DOMO E INVOCAR LOS METODOS DEL OBJETOS ANALYZER
+ 
